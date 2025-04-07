0410251398a8abecd04a93cb504e3f9fe0e086b1
yili2325 <liyi1997119@gmail.com>
2025-04-08 07:22:04 +1000

    fix db path for render deployment

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const connectDB = require('./config/db');
+const connectDB = require('./server/config/db');
 const path = require('path');
 const fs = require('fs');
 const http = require('http');
